398129d27aa2268922eb2576e0ab1fb8ad46f80c
Chuk <chukmunnlee@gmail.com>
2023-01-30 16:55:55 +0800

    day35 setup

---
+++ day35/server/src/main/java/vttp2022/csf/day35/Day35Application.java
@@ -0,0 +1,13 @@
+package vttp2022.csf.day35;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day35Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day35Application.class, args);
+	}
+
+}
---
+++ day35/server/src/test/java/vttp2022/csf/day35/Day35ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.csf.day35;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day35ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
