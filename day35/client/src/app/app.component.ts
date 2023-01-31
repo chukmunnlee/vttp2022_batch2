d5c36028160897a23afa674f872231908d3f59f8
Chuk <chukmunnlee@gmail.com>
2023-01-31 11:50:33 +0800

    day35 progamatically binding property and events

--- day35/client/src/app/app.component.ts
+++ day35/client/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SearchComponent } from './components/search.component';
+import { DisplayComponent } from './componets/display.component';
 import { Game } from './models';
 
 @Component({
@@ -6,9 +9,34 @@ import { Game } from './models';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild(SearchComponent)
+  searchComp!: SearchComponent
+
+  @ViewChild(DisplayComponent)
+  displayComp!: DisplayComponent
 
   games: Game[] = []
+
+  onResults$!: Subscription
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    // manually subscribe to the onResults event - @Output
+    this.onResults$ = this.searchComp.onResults.subscribe(
+      (games: Game[]) => {
+        console.info('>>>> in subscription')
+        // manually assign the new value to the attribute - @Input()
+        this.displayComp.games = games
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+      this.onResults$.unsubscribe()
+  }
 
   onResults(games: Game[]) {
     this.games = games
